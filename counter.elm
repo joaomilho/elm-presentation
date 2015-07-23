c600df1b8988512dc43036e22a10b5809e072c4e
Juan Lulkin <juan.lulkin@klarna.com>
2015-07-24 01:12:29 +0300

    Adds MUV skeleton

--- counter.elm
+++ counter.elm
@@ -1,3 +1,11 @@
 import Html exposing (h1, div, button, text)
 import Html.Events exposing (onClick)
 import StartApp
+
+update action count =
+  -- todo
+
+view address count =
+  -- todo
+
+main = StartApp.start { model = 0, update = update, view = view }
