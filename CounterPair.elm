c7d4835e362990bed1906141e097f4b1684b1c24
Juan Lulkin <juan.lulkin@klarna.com>
2015-07-24 23:30:26 +0300

    Adds MUV skeleton for counter pair

--- CounterPair.elm
+++ CounterPair.elm
@@ -4,3 +4,12 @@ import Counter
 import Array.Helpers exposing(get, set, array)
 import Html exposing (div)
 import StartApp
+
+update action counts =
+  -- todo
+
+view address counts =
+  -- todo
+
+main = StartApp.start { model = ???, update = update, view = view }
+
