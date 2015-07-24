511eb77332735a9c636d22765cea938f1508a036
Juan Lulkin <juan.lulkin@klarna.com>
2015-07-24 23:32:12 +0300

    Implements view of counter pair

--- CounterPair.elm
+++ CounterPair.elm
@@ -5,11 +5,16 @@ import Array.Helpers exposing(get, set, array)
 import Html exposing (div)
 import StartApp
 
+type Action = Counter1 Counter.Action | Counter2 Counter.Action
+
 update action counts =
   -- todo
 
 view address counts =
-  -- todo
+  div []
+    [ Counter.view (Signal.forwardTo address Counter1) (counts `get` 0)
+    , Counter.view (Signal.forwardTo address Counter2) (counts `get` 1)
+    ]
 
 main = StartApp.start { model = array [0, 0], update = update, view = view }
 
